4021389c6dc3105decc0ed5684f1a371998e7101
ratnakarmaker <73543192+ratnakarmaker@users.noreply.github.com>
2021-02-06 21:07:48 +0600

    first

--- app.js
+++ app.js
@@ -35,28 +35,3 @@ const renderCountryInfo = country => {
 
     `
 }
-
-
-    
-    //for loop diye
-    // for (let i = 0; i < countries.length; i++) {
-    //     const country = countries[i];
-    //     const countryDiv = document.createElement('div');
-    //     countryDiv.className = 'country';
-
-    //         const countryInfo = `
-    //         <h3 class="country_name">${country.name} </h3>
-    //         <p>${country.capital} </p>
-    //         `;
-    //         countryDiv.innerHTML = countryInfo;
-    //     countriesDiv.appendChild(countryDiv);
-    // }
-
-        //ager kora
-        // const h3 = document.createElement('h3');
-        // h3.innerText = country.name;
-        // countryDiv.appendChild(h3);
-
-        // const p = document.createElement('p');
-        // p.innerText = country.capital;
-        // countryDiv.appendChild(p);
